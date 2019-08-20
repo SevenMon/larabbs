d54cf6b940f9b83ecf7adf40557a483ebac29ed7
SevenMon <57867782@qq.com>
2019-08-20 16:54:19 +0800

    sdf

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,13 +8,13 @@
 
         <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
             <div class="card ">
-                <img class="card-img-top" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566228627614&di=ee8a15a12cf296ff1c28fae502b484d1&imgtype=0&src=http%3A%2F%2Fimg3.duitang.com%2Fuploads%2Fitem%2F201504%2F21%2F20150421H4819_J4XQr.png" alt="{{ $user->name }}">
+                <img class="card-img-top" src="{{ $user->avatar }}" alt="{{ $user->name }}">
                 <div class="card-body">
                     <h5><strong>个人简介</strong></h5>
                     <p>{{ $user->introduction }}</p>
                     <hr>
                     <h5><strong>注册于</strong></h5>
-                    <p>{{ $user->create_at->diffForHumans() }}</p>
+                    <p>{{ $user->created_at->diffForHumans() }}</p>
                 </div>
             </div>
         </div>
